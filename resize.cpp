92675519a90bf8f89fc927f27baafe2a711ac212
muskvr <muskvr@umich.edu>
2025-02-12 17:01:20 -0500

    fixed memory leak

--- resize.cpp
+++ resize.cpp
@@ -20,41 +20,43 @@ if(argc!=4 && argc!=5){
   return 1;
 }
 
-Image *image = new Image;
+Image image;
 
-std::ifstream in_file(argv[1]);
+ifstream in_file(argv[1]);
 
-if (!in_file.is_open()) {
+if (!in_file) {
     cout << "Error: Could not open input file " << argv[1] << endl;
     return 1;
 }
 
-std::ofstream out_file(argv[2]);
-if (!out_file.is_open()) {
+ofstream out_file(argv[2]);
+if (!out_file) {
     cout << "Error: Could not open output file " << argv[2] << endl;
     return 1;
 }
-Image_init(image, in_file);
+Image_init(&image, in_file);
+
 int argv3 = stoi(argv[3]);
-if(argv3 <= 0 || argv3 > Image_width(image)){
+if(argv3 <= 0 || argv3 > Image_width(&image)){
   cout << "Usage: resize.exe IN_FILENAME OUT_FILENAME WIDTH [HEIGHT]\n"
      << "WIDTH and HEIGHT must be less than or equal to original" << endl;
   return 1;
 }
-
+int argv4 = Image_height(&image);
 if(argc==5){
-  int argv4 = stoi(argv[4]);
-  if(argv4 <= 0 || argv4 > Image_height(image)){
+  argv4 = stoi(argv[4]);
+  if(argv4 <= 0 || argv4 > Image_height(&image)){
     cout << "Usage: resize.exe IN_FILENAME OUT_FILENAME WIDTH [HEIGHT]\n"
      << "WIDTH and HEIGHT must be less than or equal to original" << endl;
   return 1;
   }
-  seam_carve(image, argv3, argv4);
+
+  seam_carve(&image, argv3, argv4);
 }
 else{
-  seam_carve_width(image, argv3);
+  seam_carve_width(&image, argv3);
 }
-Image_print(image, out_file);
+Image_print(&image, out_file);
 return 0;
 }
 //Check command line arguments (using argc and argv)
